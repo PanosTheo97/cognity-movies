c62db6504cdb3c0187191d8724c89072a7726fb6
PanosTheo97 <ptheodos@arx.net>
2023-02-03 11:47:08 +0200

    Project/pods initialization

---
+++ Cognity Movies/Cognity Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cognity Movies
+//
+//  Created by Panagiotis Theodosiadis on 3/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cognity Movies/Pods/Target Support Files/Pods-Cognity Movies/Pods-Cognity Movies-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cognity_Movies : NSObject
+@end
+@implementation PodsDummy_Pods_Cognity_Movies
+@end
